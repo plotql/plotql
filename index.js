b1278afd33ce226c763aa9cfaac60d5e3fb356d9
Daniel O'Quinn <danieljoquinn@gmail.com>
2017-03-22 01:15:59 -0400

    inital commit

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
 "use strict";
+exports.mongo = require('./lib/unfreeze.js')
 
-var say = require('say.js').dev;
-
-var socketio = require('./lib/unfreeze.js');
-
-var kue = require('kue');
-var q = kue.createQueue();
-
-
-var server = require('./server')
-
-var io = socketio.listen(server)
-
-
-function tick(){
-  q.create('new_message',{ timer: new Date()}).save( function(err){
-    console.log(err)
-  })
-  io.sockets.emit('chat', (new Date()).toLocaleString());
-  setTimeout(tick, 1000)
-}
-
-io.sockets.on('connection', function(socket){
-  say("[Client connected]")
-  tick()
-});
-
+// module.exports  = plotql
